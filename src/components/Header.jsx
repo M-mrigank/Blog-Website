610aaed05a4772d831fefc81a749da15f81386be
M-mrigank <mrigank.mgr@example.com>
2023-09-05 20:03:48 +0530

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='w-full fixed top-0 bg-white'>
+      <header className='w-full border shadow-md flex items-center justify-center py-4'>
+        <h1 className='text-3xl font-bold'>CODEHELP BLOGS</h1>
+      </header>
+    </div>
+  )
+}
+
+export default Header
